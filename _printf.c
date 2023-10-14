976847156c6b5ef53ca1bcbadea4cc34ffdbd695
MicrQ <mail.micrq@proton.me>
2023-10-14 15:14:38 -0400

    task 0 of _printf project

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _printf - a function that is like the standard library of C (printf)
+ * @format: any kind of type
+ * Return: the number of characters printed to stdout
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	size_t i, j, count = 0;
+	char *str;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else
+		{
+			switch (format[i + 1])
+			{
+				case '%':
+					_putchar('%');
+					break;
+				case 'c':
+					_putchar(va_arg(args, int));
+					break;
+				case '\0':
+					va_end(args);
+					return (-1);
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					for (j = 0; str[j] != '\0'; j++)
+						_putchar(str[j]);
+					break;
+				default:
+					va_end(args);
+					return (-1);
+					break;
+			}
+			i++;
+		}
+		count++;
+	}
+	va_end(args);
+	return (count);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
