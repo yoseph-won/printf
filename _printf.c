c2c9eec38953a89bfdf30ca97572ec51f2a8af84
MicrQ <mail.micrq@proton.me>
2023-10-15 16:08:55 -0400

    newly modified _printf

--- _printf.c
+++ _printf.c
@@ -8,51 +8,46 @@
 
 int _printf(const char *format, ...)
 {
-        int count = 0, counter;
-        va_list list;
+	int count = 0, i, counter = 0;
+	va_list args;
 	char *str;
 
-        if (format == NULL)
-                return (-1);
-        va_start(list, format);
-        while (*format)
-        {
-                if (*format != '%')
-                {
-                        write(1, format, 1);
-                        count++;
-                }
-                else
-                {
-                        format++;
-                        if (*format == '\0')
-                                break;
-                        if (*format == '%')
-                        {
-                                write(1, format, 1);
-                                count++;
-                        }
-                        else if (*format == 'c')
-                        {
-                                char character = va_arg(list,int);
-                                write(1, &character, 1);
-                                count++;
-                        }
-                        else if (*format == 's')
-                        {
-                                str = va_arg(list, char*);
-                                counter = 0;
-                                while (str[counter] != '\0')
-                                        counter++;
-
-                                write(1, str, counter);
-                                count += counter;
-                        }
-                }
-                format++;
-        }
-        va_end(list);
-	return(count);
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else
+		{
+			i++;
+			switch (format[i])
+			{
+				case '\0':
+					break;
+				case '%':
+					_putchar('%');
+					break;
+				case 'c':
+					_putchar(va_arg(args, int));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					while (str[counter] != '\0')
+					{
+						_putchar(str[counter++]);
+					}
+					count += counter - 1;
+					break;
+				default:
+					break;
+			}
+		}
+		count++;
+	}
+	va_end(args);
+	return (count);
 }
 
 
